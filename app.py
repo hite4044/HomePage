a45cce4048e4a5eaa1adef76ecc6a4015c376351
hite404 <hite404@qq.com>
2025-11-01 20:28:00 +0800

    feat: 缩小背景图片, 更改颜色

--- app.py
+++ app.py
@@ -816,4 +816,4 @@ if __name__ == '__main__':
         # 设置环境变量，使得 GitHub Pages 能够正确运行
         os.environ['FLASK_APP'] = 'app.py'
         # 在开发环境中运行
-        app.run(debug=True, host='0.0.0.0', port=5000)
+        app.run(debug=True, host='0.0.0.0', port=5001)
